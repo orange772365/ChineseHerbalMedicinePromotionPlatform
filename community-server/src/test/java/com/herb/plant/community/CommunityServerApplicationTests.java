d8376959b71d6b632734045b2c7d7be0cf9eaf8d
一五一十 <2418904067@qq.com>
2023-03-29 23:03:17 +0800

    项目初始化
    https://github.com/orange772365/ChineseHerbalMedicinePromotionPlatform.git

---
+++ community-server/src/test/java/com/herb/plant/community/CommunityServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.herb.plant.community;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommunityServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
